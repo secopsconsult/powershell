8e4da9a3d6b3de9717dc46b071d5b08982ca5870
secopsconsult <github@secopsconsulting.com>
2017-03-10 12:00:09 +0000

    Whitespace clean up Removed useless $i references.

--- Get-WifiCreds.ps1
+++ Get-WifiCreds.ps1
@@ -1,29 +1,24 @@
 <#
 
    .Synopsis
-
       Command to List Wifi network connections that have been saved, their key.
 
    .DESCRIPTION
-
       Command that goes through each saved Wifi Connection and lists all saved connections, their type and the key.
 
    .EXAMPLE
-
         get-WifiCreds
-
             Lists the Wifi Networks and the keys.
 
-   #>
+#>
 
  
 
 function get-WifiCreds
-
 {
 
-    [CmdletBinding()]
-    [OutputType([string])]
+     [CmdletBinding()]
+     [OutputType([string])]
  
      Param
      (
@@ -35,32 +30,34 @@ function get-WifiCreds
 
      Process
      {
-       # Run the Netsh command to determine all wifi profiles.
-       $Output = netsh wlan show profiles
+         # Run the Netsh command to determine all wifi profiles.
+         $Output = netsh wlan show profiles
 
-       $SSID = $Output | Select-String -Pattern 'All User Profile'
-       $i = 0
+         $SSID = $Output | Select-String -Pattern 'All User Profile'
 
-       Foreach ($sid in $SSID)
-       {
-            $i ++
-            $out = ($sid -split ":")[-1].Trim() -replace '"'
-            $profile = netsh wlan show profiles name=$out key=clear
-            $pw = $profile | select-string -Pattern "Authentication"
+         Foreach ($sid in $SSID)
+         {
+              $out = ($sid -split ":")[-1].Trim() -replace '"'
+              $profile = netsh wlan show profiles name=$out key=clear
+              $pw = $profile | select-string -Pattern "Authentication"
 
-            if ($pw) {
-                $pw2 = ($pw -split ":")[-1].Trim() -replace '"'
-                if ($pw2 -eq "Open") {
-                    Write-Output "$out, <open>"
-                } else {
-                    $key = $profile | select-string -Pattern "Key Content"
-                    if ($key) {
-                        $key = ($key -split ":")[-1].Trim() -replace '"'
-                    }
-                    write-Output "$out, $pw2, $key"
-                }
-            }
-       }
+              if ($pw) 
+              {
+                  $pw2 = ($pw -split ":")[-1].Trim() -replace '"'
+                  if ($pw2 -eq "Open") 
+                  {
+                      Write-Output "$out, <open>"
+                  } else 
+                  {
+                      $key = $profile | select-string -Pattern "Key Content"
+                      if ($key) 
+                      {
+                          $key = ($key -split ":")[-1].Trim() -replace '"'
+                      }
+                      write-Output "$out, $pw2, $key"
+                  }
+              }
+         }
      }
 
 
